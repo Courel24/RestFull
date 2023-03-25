856f13b4ab47de10afd21f61ce563961b5793d84
Juan Felipe Mejia <juanmeef@unisabana.edu.co>
2023-03-25 01:04:49 -0500

    Fix: improve repositories

--- example-rest-2023-1-main/src/main/java/com/example/demo/repository/ClientRepository.java
+++ example-rest-2023-1-main/src/main/java/com/example/demo/repository/ClientRepository.java
@@ -7,10 +7,10 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Integer> {
-
+/*
     @Query("SELECT c.DOCUMENT, c.NAME, p.ID, p.NAME FROM CLIENT c INNER JOIN PET p ON c.DOCUMENT = p.CLIENT WHERE c.DOCUMENT = :document")
     Client getClientPets(@Param("document") int document);
 
     @Query("SELECT c.DOCUMENT, c.NAME, b.ID FROM CLIENT c INNER JOIN BOOKING b ON c.DOCUMENT = p.CLIENT_ID WHERE c.DOCUMENT = :document")
-    Client getClientBookings(@Param("document") int document);
+    Client getClientBookings(@Param("document") int document);*/
 }
