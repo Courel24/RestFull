bf1d7c26eed5e4b4d413c05bd401afd45af6b735
AngelG14 <angeldagosa@gmail.com>
2023-05-25 11:25:30 -0500

    Test: Docker build

--- Notifier/src/main/java/com/example/demo/NotifierApplication.java
+++ Notifier/src/main/java/com/example/demo/NotifierApplication.java
@@ -1,11 +1,13 @@
 package com.example.demo;
 
+import lombok.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NotifierApplication {
 
+	@Generated
 	public static void main(String[] args) {
 		SpringApplication.run(NotifierApplication.class, args);
 	}
